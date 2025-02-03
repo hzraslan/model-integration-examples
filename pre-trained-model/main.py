ef69689667b73b0eb5f0aafdaed996decb185cae
hizir aslan <hiziraslan@hizirs-MacBook-Air.local>
2025-02-02 20:09:58 -0500

    cleanup

--- pre-trained-model/main.py
+++ pre-trained-model/main.py
@@ -6,4 +6,5 @@ pipe = pipeline("automatic-speech-recognition", model="hzraslan/wsper")
 def inference(speech_file):
   return pipe(speech_file)["text"]
 
+
 gr.Interface(inference,gr.Audio(source="microphone", type="filepath"),"text").launch(share=True)
